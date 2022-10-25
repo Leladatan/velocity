4061bf42738a7eaf5521dfd951c3df6dc2485ea3
Vladimir <vov4ik.samurai@yandex.ru>
2022-10-25 18:36:53 +0300

    mda

---
+++ src/main.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./style.css");
+const typescript_svg_1 = __importDefault(require("./typescript.svg"));
+const counter_1 = require("./counter");
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescript_svg_1.default}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`;
+(0, counter_1.setupCounter)(document.querySelector('#counter'));
