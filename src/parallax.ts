a4a2620794ac417c55cc60df30cc802181a25e6e
Vladimir <vov4ik.samurai@yandex.ru>
2022-10-25 22:50:38 +0300

    delete ts

--- src/parallax.ts
+++
@@ -1,9 +0,0 @@
-(() => {
-    const parallax_bg = document.querySelector('.parallax-bg') as HTMLDivElement;
-    window.addEventListener('mousemove', function (event) {
-        const x: number = event.clientX / window.innerWidth;
-        const y: number = event.clientY / window.innerHeight;
-        const parallaxSpeed = parallax_bg!.getAttribute('data-speedParallax');
-        parallax_bg!.style.transform = `translate(-${x} * ${parallaxSpeed}px, -${y} * ${parallaxSpeed}px)`;
-    });
-})();
